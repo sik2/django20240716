7fbaea46c757a0e9d3b68ac95d9fffd2ef90e47f
sik2 <bickwang123@gmail.com>
2024-07-18 17:39:29 +0900

    24

--- pybo/urls.py
+++ pybo/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('question/', views.question),
+    path('<int:question_id>/', views.detail),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
 
-def question(request):
-    return HttpResponse("질문 목록")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
